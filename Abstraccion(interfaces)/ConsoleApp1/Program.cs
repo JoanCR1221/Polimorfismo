f6b02e73f3138e82014cc03e13d0f9448a81f176
JoanCR1221 <162819627+JoanCR1221@users.noreply.github.com>
2024-03-09 15:31:42 -0600

    adstracio

---
+++ Abstraccion(interfaces)/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+ public abstract class Vehicle
+{
+    public int MaxSpeed { get; set; }
+    public int Acceleration { get; set; }
+
+    public abstract void Move();
+}
+public class Car : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("El carro se esta miviendo");
+    }
+}
+
+public class Bike : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("La bisicleta se esta moviendo");
+    }
+}

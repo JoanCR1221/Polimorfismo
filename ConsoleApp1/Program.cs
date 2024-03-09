2e63255c8c797cbea5db960c20148c33466196e5
JoanCR12211 <jandree1221.gmail.com>
2024-03-09 11:11:06 -0600

    listl

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Hello, World Polimorfismo!");
+
+public class Animal
+{
+    public virtual void HacerSonido()
+    {
+        Console.WriteLine("El animal hace un sonido");
+    }
+}
+
+public class Perro : Animal
+{
+    public override void HacerSonido()
+    {
+        Console.WriteLine("El perro ladra");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal miAnimal = new Animal();
+        Animal miPerro = new Perro();
+
+        miAnimal.HacerSonido(); // Salida: El animal hace un sonido
+        miPerro.HacerSonido(); // Salida: El perro ladra
+    }
+}
